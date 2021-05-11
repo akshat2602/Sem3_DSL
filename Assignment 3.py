41824d23bf0ec12cbdcbeb8bd2aadb1211a8919d
akshat2602 <akshatsharma2602@gmail.com>
2021-05-11 06:47:47 +0530

    Fixed assignment 3 and 6

--- Assignment 3.py
+++ Assignment 3.py
@@ -1,117 +1,103 @@
 class Matrix:
-	mat=[]
+    def __init__(self, m, n):
+        self.m = m
+        self.n = n
+        self.l = [[0 for i in range(n)] for j in range(m)]
+        self.chck = 0
 
-	def __init__(self,name):
-		self.name=name
-		print("For matrix",self.name)
-		self.m=int(input("enter no. of rows"))
-		self.n=int(input("enter no. of column"))
-		self.mat=[]
-		rowmat=[]
-		for i in range(self.m):
-			for j in range(self.n):
-				e=int(input("Enter the element i[%d][%d]"%(i+1,j+1)))
-				rowmat.append(e)
-			self.mat.append(rowmat)
-			rowmat=[]
-	
-	def addMat(self,m1,m2):
-		addMatrix=[]
-		rowmat=[]
-		for i in range(m1.m):
-			for j in range(m1.n):
-				e=m1.mat[i][j]+m2.mat[i][j]
-				rowmat.append(e)
-			addMatrix.append(rowmat)
-			rowmat=[]
-		return addMatrix
-	
-	def subMat(self,m1,m2):
-		subMatrix=[]
-		rowmat=[]
-		for i in range(m1.m):
-			for j in range(m1.n):
-				e=m1.mat[i][j]-m2.mat[i][j]
-				rowmat.append(e)
-			subMatrix.append(rowmat)
-			rowmat=[]
-		return subMatrix
-	
-	def multMat(self,m1,m2):
-		multMatrix=[]
-		rowmat=[]
-		sum=0
-		for i in range(m1.m):
-			for j in range(m2.n):
-				for k in range(m2.n):
-					sum+=m1.mat[i][k]*m2.mat[k][j]
-				rowmat.append(sum)
-				sum=0
-			multMatrix.append(rowmat)
-			rowmat=[]
-		return multMatrix
-				
-	def transpose(self,mat):
-		new=[]
-		rowmat=[]
-		for i in range(mat.m):
-			for j in range(mat.n):
-				e=mat.mat[j][i]
-				rowmat.append(e)
-			new.append(rowmat)
-			rowmat=[]
-		return new
-		print(new)
-	
+    def getmat(self):
+        print("Enter the matrix: ")
+        for i in range(self.m):
+            x = True
+            while x:
+                l1 = list(map(int, input().split()))
+                if len(l1) != self.n:
+                    print("Invalid Input!! Enter the row again.")
+                    continue
+                else:
+                    x = False
+                    for j in range(self.n):
+                        self.l[i][j] = l1[j]
 
-def compare(m,n,p,q):
-	if (m==p and n==q):
-		return 1
-	else:
-		return 0
+    def write(self):
+        if self.chck != 0:
+            print("Invalid operation!")
+        else:
+            for i in range(self.m):
+                for j in range(self.n):
+                    print(self.l[i][j], end=" ")
+                print()
 
-def compareMult(n,p):
-	if (n==p):
-		return 1
-	else:
-		return 0
-	
-again="y"
-while again=="Y" or again=="y":
-		mat1=Matrix(1)
-		mat2=Matrix(2)
-		m=mat1.m
-		n=mat1.n
-		p=mat2.m
-		q=mat2.n
-		opAgain="y"
-		while opAgain=="Y" or opAgain=="y":
-			option=int(input("Enter\n1) For addition\n2) For subraction\n3) For multiplication\n4) For transpose"))
-			if option==1:
-				check=compare(m,n,p,q)
-				if check:
-					print("Addition of matrices is",Matrix.addMat(mat1,mat2))
-				else:
-					print("cannot be addedd")
-					
-			elif option==2:
-				check=compare(m,n,p,q)
-				if check:
-					print("Subtraction of matrices is",Matrix.subMat(mat1,mat2))
-				else:
-					print("cannot be subtracted")
-					
-			elif option==3:
-				check=compareMult(n,p)
-				if check:
-					print("Multiplication of matrices is",Matrix.multMat(mat1,mat2))
-				else:
-					print("cannot be multiplied")
-					
-			elif option==4:	
-				print("Transpose of matrix 1 is",Matrix.transpose(mat1))
-				print("Transpose of  matrix 2 is",Matrix.transpose(mat2))
-			else:
-				print("Wrong input")
-			opAgain=input("want to perform other operations? (Y/N)")
-		again=input("want to perform operation of different matrices? (Y/N)")
+    def addmat(self, m1):
+        mx = Matrix(self.m, self.n)
+        if m1.m != self.m and m1.n != self.n:
+            mx.chck = 1
+            return mx
+        for i in range(self.m):
+            for j in range(self.n):
+                mx.l[i][j] = self.l[i][j] + m1.l[i][j]
+        return mx
+
+    def submat(self, m1):
+        mx = Matrix(self.m, self.n)
+        if m1.m != self.m and m1.n != self.n:
+            mx.chck = 1
+            return mx
+        for i in range(self.m):
+            for j in range(self.n):
+                mx.l[i][j] = self.l[i][j] - m1.l[i][j]
+        return mx
+
+    def mulmat(self, m1):
+        mx = Matrix(self.m, m1.n)
+        if self.n != m1.m:
+            mx.chck = 1
+            return mx
+        for i in range(mx.m):
+            for j in range(mx.n):
+                for k in range(self.n):
+                    mx.l[i][j] += self.l[i][k] * m1.l[k][j]
+        return mx
+
+    def transpose(self):
+        mx = Matrix(self.n, self.m)
+        for i in range(self.n):
+            for j in range(self.m):
+                mx.l[i][j] = self.l[j][i]
+        return mx
+
+
+m, n = map(int, input("Enter the dimensions of the first Matrix separated by whitespace: ").split())
+m1 = Matrix(m, n)
+m1.getmat()
+x, y = map(int, input("Enter the dimensions of the second Matrix separated by whitespace: ").split())
+m2 = Matrix(x, y)
+m2.getmat()
+while True:
+    print("-" * 150)
+    print("Enter your choice number from the following")
+    print("1. Addition of two matrices\n2. Subtraction of two matrices\n3. Multiplication of two matrices\n4. "
+          "Transpose of a matrix\n5. Re-enter the matrices.\n6. Exit.")
+    ch = int(input(">>"))
+    if ch == 1:
+        m1.addmat(m2).write()
+    elif ch == 2:
+        m1.submat(m2).write()
+    elif ch == 3:
+        m1.mulmat(m2).write()
+    elif ch == 4:
+        print("Transpose of first Matrix: ")
+        m1.transpose().write()
+        print("Transpose of second Matrix: ")
+        m2.transpose().write()
+    elif ch == 5:
+        m, n = map(int, input("Enter the dimensions of the Matrix separated by whitespace: ").split())
+        m1 = Matrix(m, n)
+        m1.getmat()
+        x, y = map(int, input("Enter the dimensions of the second Matrix separated by whitespace: ").split())
+        m2 = Matrix(x, y)
+        m2.getmat()
+    elif ch == 6:
+        break
+    else:
+        print("Invalid Input")
--- Assignment 6.py
+++ Assignment 6.py
@@ -1,28 +1,48 @@
-class sort:
-    def __init__(self):
-        self.arr = []
-        self.n = int(input("Enter number of students:"))
-        print("Enter the first year percentages of the students: ")
-        for _ in range(self.n):
-            self.arr.append(int(input()))        
-        
-    def display(self):
-        for i in range (self.n):
-            print(self.arr[i])
+class StudMarks:
+    def __init__(self, l):
+        self.l = l
+        self.size = 0
+        for _ in l:
+            self.size += 1
 
-    def quickSort(self):
-        pass
+    @staticmethod
+    def partition(l, a, b):
+        i = a - 1
+        pvt = l[b]
+        for j in range(a, b):
+            if l[j] < pvt:
+                i += 1
+                l[i], l[j] = l[j], l[i]
+        l[i + 1], l[b] = l[b], l[i + 1]
+        return i + 1
+
+    def qcksort(self, l, a, b):
+        l1 = l[:]
+        if a < b:
+            pt = self.partition(l1, a, b)
+            l1 = self.qcksort(l1, a, pt - 1)
+            l1 = self.qcksort(l1, pt + 1, b)
+        return l1
+
+    def quick(self):
+        return self.qcksort(l, 0, self.size - 1)
 
 
-flag = True
-while(flag):
-    choice = int(input("1) Quick sort \n2) Exit"))
-    S = sort()
-    if choice == 1:
-        S.quickSort()
-        S.display()
-    elif choice == 2:
-        flag = False
+l = list(map(float, input("Enter the first year percentages of students separated by whitespaces: ").split()))
+s = StudMarks(l)
+while True:
+    print("-" * 150)
+    l1 = s.quick()
+    for i in range(s.size - 1, -1, -1):
+        if s.size - i > 5:
+            break
+        print(str(s.size - i) + ". " + str(l1[i]))
+        i += 1
+    print(l1)
+    print("Re-Enter percentages of students? (Y/N)")
+    ch = input(">>>")
+    if ch == "Y":
+        l = list(map(float, input("Enter the second year percentages of students separated by whitespaces: ").split()))
+        s = StudMarks(l)
     else:
-        print("Invalid choice! Exiting now.")
-        flag = False
+        break
